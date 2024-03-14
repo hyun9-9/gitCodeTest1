8a21a3c02e33233cda1c670f6fbe9d565f17dd51
hyun9-9 <pwc1088@naver.com>
2024-03-14 22:33:13 +0900

    test

---
+++ main/java/com/example/test01/Test011Application.java
@@ -0,0 +1,13 @@
+package com.example.test01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test011Application.class, args);
+	}
+
+}
---
+++ test/java/com/example/test01/Test011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
